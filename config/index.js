50e71716ab3bb9c97af954fe46add19b4831d88c
Adriano Matsuo Da Silva <asi258@brrva1lm612604.nextel.com.br>
2019-07-30 21:10:20 -0300

    Add Redis for caching APIs informations

--- config/index.js
+++ config/index.js
@@ -5,6 +5,16 @@ const config = {
   spotifyApi: {
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+  },
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    openWeather: {
+      timeout: 1800
+    },
+    spotify: {
+      timeout: 1200
+    }
   }
 }
 
