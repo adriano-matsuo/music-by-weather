9aac0ed98db7f11e9148a878f47e2691f08b993d
Adriano Matsuo Da Silva <asi258@brksk1l10674152.nextel.com.br>
2019-08-02 17:30:11 -0300

    Change Redis config to accept url instead of host and port

--- config/index.js
+++ config/index.js
@@ -7,8 +7,7 @@ const config = {
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET
   },
   redis: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+    url: process.env.REDIS_URL,
     openWeather: {
       timeout: 1800
     },
